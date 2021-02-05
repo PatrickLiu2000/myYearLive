7b592b8609696e58e45293b2370a4adf98320dac
Patrick Liu <patrickliu2000@gmail.com>
2021-02-05 14:07:53 -0500

    touched up will's merge

--- App.js
+++ App.js
@@ -1,64 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-community/google-signin';
-
-GoogleSignin.configure({
-  webClientId: '118502184356-0j38lkdstkbiqvq0d2hlpfcn9dj9fsbl.apps.googleusercontent.com',
-});
-async function onGoogleButtonPress() {
-  // Get the users ID token
-  const { idToken } = await GoogleSignin.signIn();
-
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-}
-
-function Login() {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  // Handle user state changes
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
-
-  if (initializing) return null;
-
-  if (!user) {
-    return (
-      <View>
-        <Text>Invalid User!</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View>
-      <Text style = {styles.remember}>Welcome {user.email}</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import LoginView from './components/login/LoginView';
 
 export default function App() {
   return (
     <View style={containerStyle.container}>
       <View style={childStyle.container}>
-      <Login/>
-      <Button
-      title="Google Sign-In"
-      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-      style={styles.login_button}/>
+      <LoginView/>
       </View>
     </View>
   );
--- components/login/LoginView.js
+++ components/login/LoginView.js
@@ -1,5 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
 import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-community/google-signin';
 import { 
     Platform,
     Button, 
@@ -30,6 +31,21 @@ async function onEmailSignInButton(email, password) {
     
         console.error(error);
     });
+}
+
+
+GoogleSignin.configure({
+    webClientId: '118502184356-0j38lkdstkbiqvq0d2hlpfcn9dj9fsbl.apps.googleusercontent.com',
+  });
+async function onGoogleButtonPress() {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
 }
 
 export default function LoginView() {
@@ -132,10 +148,14 @@ export default function LoginView() {
 
         {/*FORGOT PASS*/}
         <TouchableOpacity>
-            <Text>Login</Text>
             <Text style={styles.forgot_button}>Forgot Password?</Text>
         </TouchableOpacity>
-        
+        <TouchableOpacity
+        title="Google Sign-In"
+        onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        style={styles.login_button}>
+            <Text style={styles.loginText}>Google Log In</Text> 
+        </TouchableOpacity>
 
     </View>   
     )
