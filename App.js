43faf251dba424113d394c26165c573046f2b4a9
Patrick Liu <patrickliu2000@gmail.com>
2021-02-05 12:35:33 -0500

    removed unneeded login components, styled google login

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import LoginView from './components/login/LoginView';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-community/google-signin';
 
@@ -18,15 +17,48 @@ async function onGoogleButtonPress() {
   return auth().signInWithCredential(googleCredential);
 }
 
+function Login() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Invalid User!</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text style = {styles.remember}>Welcome {user.email}</Text>
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <View style={containerStyle.container}>
       <View style={childStyle.container}>
+      <Login/>
       <Button
       title="Google Sign-In"
       onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-      />
-        <LoginView/>
+      style={styles.login_button}/>
       </View>
     </View>
   );
@@ -36,7 +68,7 @@ const containerStyle = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#bbdfc8',
-    //alignItems: 'center',
+    alignItems: 'center',
     justifyContent: 'center'
   },
 });
@@ -48,3 +80,60 @@ const childStyle = StyleSheet.create({
   }
 })
 
+const styles = StyleSheet.create({
+  container: {
+      flex: 1, 
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+  },
+
+  text_footer: {
+      color: "#008891",
+      fontWeight: "bold",
+      fontSize: 18,
+  },
+
+  textInput: {
+      flex: 1,
+      marginTop: 20,
+      paddingLeft: 10, 
+      fontSize: 14,
+  },
+
+  remember:{
+      padding: 5,
+      fontWeight: "bold",
+      color: "#008891",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: 18
+  },
+
+  login_button: {
+      width: "50%",
+      borderRadius: 10,
+      height: 40,
+      alignItems: "center",
+      marginTop: 35,
+      justifyContent: "center",
+      backgroundColor: "#008891",
+      marginHorizontal: 70
+
+  },
+
+  loginText: {
+      fontWeight: "bold",
+      fontSize: 18,
+      color: "#fff",
+
+  },
+
+  forgot_button: {
+      height: 30,
+      marginBottom: 30,
+      marginTop: 20,
+      marginHorizontal: 90
+  }
+
+})
